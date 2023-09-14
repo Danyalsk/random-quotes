277979f79b72b7a661178db66271a0347925baec
danyal <danyalforg@gmail.com>
2023-09-14 19:02:26 +0530

    first commit

---
+++ src/Dashboard/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div id="footer">
+      <span>by Danyal</span>
+      <a rel="noopener noreferrer" href="https://github.com/loarsaw">
+       
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
